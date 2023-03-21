6abc987eb45cf3fd1441e5fd3af9a3ef2f723e0c
Arturo Coronel <contact@yayu.dev>
2023-03-20 22:00:26 -0700

    Add config for bundle analyzer on dev

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 // @ts-check
 require("dotenv");
 
-const { i18n } = require("./next-i18next.config");
-const nextMDX = require("@next/mdx");
+const withPlugins = require("next-compose-plugins");
 const prismPlugin = require("@mapbox/rehype-prism");
+const bundleAnalyzer = require("@next/bundle-analyzer");
+
+const nextMDX = require("@next/mdx");
+const { i18n } = require("./next-i18next.config");
 
 const DEV_ENV = process.env.NODE_ENV === "development";
 
@@ -12,6 +15,7 @@ const CONTENTFUL_ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
 const CONTENTFUL_PREVIEW_ACCESS_TOKEN =
   process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN;
 const VERCEL_URL = process.env.VERCEL_URL;
+const ANALYZE_BUNDLE = process.env.ANALYZE_BUNDLE;
 
 let SERVER_URL = process.env.SERVER_URL || "https://yayu.dev";
 let API_ALLOWED_ORIGINS = process.env.API_ALLOWED_ORIGINS || "yayu.dev";
@@ -61,4 +65,9 @@ const withMDX = nextMDX({
   },
 });
 
-module.exports = withMDX(nextConfig);
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: ANALYZE_BUNDLE === "true",
+  openAnalyzer: false,
+});
+
+module.exports = withPlugins([withMDX, withBundleAnalyzer], nextConfig);
