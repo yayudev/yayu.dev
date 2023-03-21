c6c9fa4fc2ba5a7f0e48c950884d7f140e83c920
Arturo Coronel <contact@yayu.dev>
2023-03-21 06:38:18 -0700

    tesst

--- components/blog/blog-posts-list.tsx
+++ components/blog/blog-posts-list.tsx
@@ -1,16 +1,26 @@
 import { BlogPost } from "@/types/blog-api";
 
 import { BlogPostItem } from "@/components/blog/blog-post-item";
+import { Ref, useEffect, useRef } from "react";
 
 interface BlogPostsListProps {
+  ref?: Ref<HTMLElement>;
   posts: BlogPost[];
 }
 
 export function BlogPostsList({ posts }: BlogPostsListProps) {
+  const ref = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    if (typeof window === undefined) return;
+
+    (window as any).document.querySelector("#page-content").scrollTo(0, 0);
+  }, [posts]);
+
   if (!posts.length) return null;
 
   return (
-    <section>
+    <section ref={ref}>
       {posts.map((post) => (
         <BlogPostItem key={post?.slug} post={post} />
       ))}
