d1a42d79a630a61156eda7d87dff0a69d0ebfd8e
Arturo Coronel <contact@yayu.dev>
2023-03-21 06:37:01 -0700

    tesst

--- components/blog/blog-posts-list.tsx
+++ components/blog/blog-posts-list.tsx
@@ -1,26 +1,16 @@
 import { BlogPost } from "@/types/blog-api";
 
 import { BlogPostItem } from "@/components/blog/blog-post-item";
-import { useEffect, useRef } from "react";
 
 interface BlogPostsListProps {
   posts: BlogPost[];
 }
 
 export function BlogPostsList({ posts }: BlogPostsListProps) {
-  const ref = useRef<HTMLElement>(null);
-
-  useEffect(() => {
-    if (typeof window === undefined) return;
-
-    // Not the best approach but it works
-    (window as any).document.querySelector("#page-content").scrollTo(0, 0);
-  }, [posts]);
-
   if (!posts.length) return null;
 
   return (
-    <section ref={ref}>
+    <section>
       {posts.map((post) => (
         <BlogPostItem key={post?.slug} post={post} />
       ))}
